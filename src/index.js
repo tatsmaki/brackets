a914575b3612e69fcbc37cb703ce6343201a4455
tatsmaki <61022192+tatsmaki@users.noreply.github.com>
2020-10-09 16:49:52 +0300

    feat: somewhat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj={'(': 0, ')': 0, '[': 0, ']': 0, '{': 0, '}': 0, '|': 0,};
+  for (let i of str) {
+    obj[i]++;
+    if (i===')' && obj['(']<obj[')']) return false;
+    if (i===']' && obj['[']<obj[']']) return false;
+    if (i==='}' && obj['{']<obj['}']) return false;
+  }
+  if (obj['(']!==obj[')'] || 
+      obj['[']!==obj[']'] || 
+      obj['{']!==obj['}']) return false;
+  else return true;
 }
